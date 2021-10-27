1c83614e77352bee21d181ec4f26da7897779519
Danijel Jovanović <jovanovic.pr55.2020@uns.ac.rs>
2021-10-27 12:06:53 +0200

    PA

---
+++ PRIMENJENI ALGORITMI/01 Genetic Algorithm/EquationSolverGA - Zadatak v2/main.jl
@@ -0,0 +1,35 @@
+#   PR55/2020 Danijel Jovanovic
+#   Kreirati genetiski algoritam kojim se resava jednacina
+# - 4x^2 - 6x - 3y^3 + 0.5y + 3z + 8w - 6.1u + 2v = 10
+# - Resenje traziti u intervalu brojeva [-1, 1]
+# - Ukrstanje vrsiti po parovima hromozoma, pocevsi od onih sa najmanjim fitnesom
+# - Radi bolje konvergencije, korisitit elitu sa ucescem od 20%
+
+#=
+■ Dodatni zadatak:
+– Analizirati ponašanje sistema i pronaći optimalnu vrednost elite u rasponu od
+0-30%
+– Analizirati ponašanje sistema i pronaći optimalnu vrednost procenta mutacije i
+tačke ukrštanja. Procenat mutacije analizirati u rasponu 0-30%, a tačke ukrštanja
+u rasponu 2-5
+=#
+
+include("geneticAlgorithm.jl")
+
+genesLength = 6
+populationSize = 50
+crossoverPoint = rand(2:4) 
+mutationPercentage = 0.2    
+elitePercentage = 0.2    
+repeatSize = 3             
+
+population = generatePopulation(populationSize, genesLength)
+calculatePopulationFitness!(population)
+printPopulation(population)
+
+popGen, repeatNum, population = geneticAlgorithm!(population, elitePercentage, crossoverPoint, mutationPercentage, repeatSize)
+printPopulation(population)
+
+println("\n\t\tUKUPAN BROJ GENERACIJA: $popGen")
+println("\t\tCROSSOVER POINT: $crossoverPoint")
+println("\t\tBR. PON. POSLEDNJEG BEST-FITA: $repeatNum.")
