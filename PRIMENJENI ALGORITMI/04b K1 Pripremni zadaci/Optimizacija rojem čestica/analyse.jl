5a9072758278e5ed358653efc10d44c8651618a8
Danijel Jovanović <jovanovic.pr55.2020@uns.ac.rs>
2021-11-17 16:36:57 +0100

    Fix up

--- PRIMENJENI ALGORITMI/04b K1 Pripremni zadaci/Optimizacija rojem čestica/analyse.jl
+++ PRIMENJENI ALGORITMI/04b K1 Pripremni zadaci/Optimizacija rojem čestica/analyse.jl
@@ -16,7 +16,7 @@ function findBestMaxVelocity(swarm, targetValue, maxIteration, maxVelocityRange,
     nizProseka = []
     for i in 1:num
         maxVelocity = maxVelocityRange[i]
-        avgBV = PsoAlgorithm!(swarm, targetValue, maxIteration, maxVelocity, X)
+        avgBV = calculateAverage(swarm, targetValue, maxIteration, maxVelocity, X, num)
         push!(nizProseka, avgBV)
     end
     minIdxProseka = argmin(nizProseka)[1]
